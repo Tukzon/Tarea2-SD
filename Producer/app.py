57053a0fec31039793e8e97bb7e3120f4be585bb
Tukzon <gonzalo.salazar1@mail.udp.cl>
2022-10-25 14:00:09 -0300

    intento de producer con postgres, por modificar a servicios

--- Producer/app.py
+++ Producer/app.py
@@ -3,7 +3,18 @@ import json
 from time import time
 from flask import Flask, request, jsonify
 from kafka import KafkaProducer
+from kafka.errors import KafkaError
+import psycopg2
 import asyncio
+
+# CONFIGURE POSTGRES CONNECTION
+conn = psycopg2.connect(
+    host="postgres",
+    database="tarea2",
+    user="postgres",
+    password="postgres")
+
+cursor = conn.cursor()
 
 app = Flask(__name__)
 topic_list = []
@@ -30,20 +41,27 @@ def NewMember():
     data = request.get_json()
     if not data:
         return jsonify({'message': 'No input data provided'}), 400
-    return jsonify(data), 201
+    if cursor.execute("SELECT * FROM miembros WHERE rut = %s", (data['rut'],)).fetchone():
+        return jsonify({'message': 'Member already exists'}), 400
+    cursor.execute("INSERT INTO miembros (nombre,apellido,rut,correo,patente,premium) VALUES (%s,%s,%s,%s,%s,%s)", (data['nombre'],data['appelido'],data['rut'],data['correo'],data['patente'],data['premium']))
+    conn.commit()
+    return jsonify({'message': 'Member created successfully'}), 201
 
 @app.route('/newVenta', methods=['POST'])
 def newVenta():
     data = request.get_json()
     if not data:
         return jsonify({'message': 'No input data provided'}), 400
-    return jsonify(data), 201
+    cursor.execute("INSERT INTO ventas (cliente,cantidad,hora,stock,ubicacion) VALUES (%s,%s,%s,%s,%s)", (data['cliente'],data['cantidad'],data['hora'],data['stock'],data['ubicacion']))
+    conn.commit()
+    return jsonify({'message': 'Venta created successfully'}), 201
 
 @app.route('/carritoProfugo', methods=['POST'])
 def carritoProfugo():
     data = request.get_json()
     if not data:
         return jsonify({'message': 'No input data provided'}), 400
+    cursor.execute("INSERT INTO carritos (coordenadas) VALUES (%s)", (data['coordenadas'],))
     return jsonify(data), 201
 
 if __name__== "__main__":
