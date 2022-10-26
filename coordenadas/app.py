76c03ce62019edd0a17d07171a7076731c454388
Gonzalo Salazar Muñoz <gonsalazarm@gmail.com>
2022-10-25 23:25:49 -0300

    creado consumer para coordenadas

---
+++ coordenadas/app.py
@@ -0,0 +1,34 @@
+import asyncio
+import json
+import os
+from kafka import KafkaConsumer
+import time
+
+async def consume():
+    consumer = KafkaConsumer(
+        'coordenadas',
+        bootstrap_servers='kafka:9092',
+        auto_offset_reset='earliest')
+    await consumer.start()
+    try:
+        async for msg in consumer:
+            print(msg)
+            return json.loads(msg.value)
+    finally:
+        await consumer.stop()
+
+async def coordenadas():
+    data = "NO DATA"
+    while True:
+        try:
+            data = await asyncio.wait_for(consume(), 60)
+            print(data)
+        except asyncio.TimeoutError:
+            print('timeout')
+
+
+
+if __name__ == '__main__':
+    loop = asyncio.get_event_loop()
+    loop.create_task(coordenadas())
+    loop.run_forever()
